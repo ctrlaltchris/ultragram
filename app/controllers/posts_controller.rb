6889f222e2574be84a8abb67f2fdf3209284ca70
Christopher McCorry <cmac@TurboSweetLaptop.local>
2016-06-06 14:44:27 +1000

    New post form functional, obligatory corpse.png submitted as test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,11 @@ class PostsController < ApplicationController
     def new
         @post = Post.new
     end
+    def create
+        @post = Post.create(post_params)
+        redirect_to posts_path
+    end
+    def post_params
+        params.require(:post).permit(:image, :caption)
+    end
 end
