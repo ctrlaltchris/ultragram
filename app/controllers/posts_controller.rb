1da7e1e47eb2f60a69b82afaae4e7b41a700304b
Christopher McCorry <cmac@TurboSweetLaptop.local>
2016-06-09 20:17:42 +1000

    Post ownership now working correctly. Adding users comments model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
     before_action :set_post, only: [:show, :edit, :update, :destroy]
     before_action :authenticate_user!
+    before_action :owned_post, only: [:edit, :update, :destroy]
+
     def post_params
         params.require(:post).permit(:image, :caption)
     end
@@ -41,4 +43,10 @@ class PostsController < ApplicationController
         flash.now[:success] = "P0ST DESTR0Y3D"
         redirect_to posts_path
     end
+    def owned_post
+        unless current_user == @post.user
+            flash[:alert] = "That post doesn't belong to you!"
+            redirect_to root_path
+    end
+end
 end
